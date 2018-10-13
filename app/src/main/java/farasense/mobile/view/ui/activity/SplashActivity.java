10ecae9d035a1a0a250d3a234a17214c6262c1f6
Tássio Lucas <tassiolucas@live.com>
2018-10-13 11:05:17 -0300

    -Estrutura a SplashScren

---
+++ app/src/main/java/farasense/mobile/view/ui/activity/SplashActivity.java
@@ -0,0 +1,8 @@
+package farasense.mobile.view.ui.activity;
+
+import farasense.mobile.view.ui.activity.base.BaseActivity;
+
+public class SplashActivity extends BaseActivity {
+
+
+}
---
+++ app/src/main/java/farasense/mobile/view/ui/activity/base/BaseActivity.java
@@ -0,0 +1,23 @@
+package farasense.mobile.view.ui.activity.base;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class BaseActivity extends AppCompatActivity {
+
+    @Override
+    public void onCreate(Bundle bundle) {
+        super.onCreate(bundle);
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+
+
+
+    }
+
+
+
+}
