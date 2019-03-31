26e09fe5b44e7354746bbc68466c2219df3be2e9
Tássio Lucas <tassiolucas@live.com>
2019-03-30 22:38:53 -0300

    Inclui localização para identificação de despositivos BLEs

---
+++ app/src/main/java/farasense/mobile/bluetooth/BleScanCallback.java
@@ -0,0 +1,53 @@
+package farasense.mobile.bluetooth;
+
+import android.bluetooth.BluetoothDevice;
+import android.bluetooth.le.ScanCallback;
+import android.bluetooth.le.ScanResult;
+import android.content.Context;
+import android.os.Build;
+import android.support.annotation.RequiresApi;
+import android.util.Log;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.UUID;
+
+@RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
+public class BleScanCallback extends ScanCallback {
+
+        private static final String FARASENSE_SENSOR_1 = "80:7D:3A:C7:22:DE";
+        private static final UUID FARASENSE_CHARACTERISTIC_UUID_SENSOR_1 = UUID.fromString("129FECFC-3F58-11E9-B210-D663BD873D93");
+        private static Context context;
+        private static HashMap<Object, Object> bleScanResults;
+
+    public BleScanCallback(HashMap<Object, Object> bleScanResults) {
+        this.bleScanResults = bleScanResults;
+    }
+
+    @Override
+    public void onScanResult(int callbackType, ScanResult result) {
+        super.onScanResult(callbackType, result);
+        addScanResult(result);
+    }
+
+    @Override
+    public void onBatchScanResults(List<ScanResult> results) {
+        super.onBatchScanResults(results);
+        for (ScanResult result : results) {
+            Log.i("ScanCallback", "Results: "+ result.toString());
+            addScanResult(result);
+        }
+    }
+
+    @Override
+    public void onScanFailed(int errorCode) {
+        super.onScanFailed(errorCode);
+        Log.e("ScanCallback", "Failed, Error Code: " + errorCode);
+    }
+
+    private void addScanResult(ScanResult result) {
+        BluetoothDevice device = result.getDevice();
+        String deviceAddress = device.getAddress();
+        this.bleScanResults.put(deviceAddress, device);
+    }
+}
--- app/src/main/java/farasense/mobile/view/ui/activity/fragment/RealTimeCurrentIndicatorView.java
+++ app/src/main/java/farasense/mobile/view/ui/activity/fragment/RealTimeCurrentIndicatorView.java
@@ -1,18 +1,12 @@
 package farasense.mobile.view.ui.activity.fragment;
 
+import android.Manifest;
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
-import android.bluetooth.BluetoothGatt;
-import android.bluetooth.BluetoothGattCallback;
 import android.bluetooth.BluetoothGattCharacteristic;
-import android.bluetooth.BluetoothGattDescriptor;
-import android.bluetooth.BluetoothGattService;
 import android.bluetooth.BluetoothManager;
-import android.bluetooth.BluetoothProfile;
 import android.bluetooth.le.BluetoothLeScanner;
-import android.bluetooth.le.ScanCallback;
 import android.bluetooth.le.ScanFilter;
-import android.bluetooth.le.ScanResult;
 import android.bluetooth.le.ScanSettings;
 import android.content.Context;
 import android.content.Intent;
@@ -25,16 +19,21 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Toast;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
-import java.util.UUID;
 import javax.annotation.Nullable;
+import farasense.mobile.bluetooth.BleScanCallback;
 import farasense.mobile.view.ui.activity.DashboardActivity;
+
+import static android.content.Context.BLUETOOTH_SERVICE;
 
 public class RealTimeCurrentIndicatorView extends View {
 
     private final static String BLE_NOT_SUPPORTED_MESSAGE = "Bluetooth Low Energy não suportado.";
     private final static int REQUEST_ENABLE_BT = 1;
+    private final static int REQUEST_FINE_LOCATION = 0;
     private static final long SCAN_PERIOD = 10000;
+    private static final String TAG = RealTimeCurrentIndicatorView.class.toString();
 
     private Context context;
     private Handler bleHandler;
@@ -44,12 +43,12 @@ public class RealTimeCurrentIndicatorView extends View {
     private ScanSettings bleSettings;
     private List<ScanFilter> bleFilters;
     private ArrayList<BluetoothDevice> devicesList;
-
-    private static final String FARASENSE_SENSOR_1 = "80:7D:3A:C7:22:DE";
-    private static final UUID FARASENSE_CHARACTERISTIC_UUID_SENSOR_1 = UUID.fromString("129FECFC-3F58-11E9-B210-D663BD873D93");
+    private HashMap<Object, Object> bleScanResults;
+    private BleScanCallback bleScanCallback;
+    private BluetoothLeScanner bleBluetoothLeScanner;
 
     private boolean bleDeviceConnected = false;
-    private boolean bleScanDevice = false;
+    private boolean bleScanning = false;
 
     @RequiresApi(api = Build.VERSION_CODES.JELLY_BEAN_MR2)
     public RealTimeCurrentIndicatorView(Context context) {
@@ -79,195 +78,94 @@ public class RealTimeCurrentIndicatorView extends View {
     private void init(Context context) {
         this.context = context;
 
-        bleHandler = new Handler();
-
         if (!context.getPackageManager().hasSystemFeature(PackageManager.FEATURE_BLUETOOTH_LE)) {
             Toast.makeText(context, BLE_NOT_SUPPORTED_MESSAGE, Toast.LENGTH_SHORT).show();
         }
 
-        BluetoothManager bluetoothManager = (BluetoothManager) getContext().getSystemService(Context.BLUETOOTH_SERVICE);
-
+        BluetoothManager bluetoothManager = (BluetoothManager) context.getSystemService(BLUETOOTH_SERVICE);
         sensorBleAdapter = bluetoothManager.getAdapter();
     }
 
+    @RequiresApi(api = Build.VERSION_CODES.M)
     @Override
     protected void onAttachedToWindow() {
         super.onAttachedToWindow();
-        if (sensorBleAdapter == null || !sensorBleAdapter.isEnabled()) {
-            Intent enableBleIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
-            ((DashboardActivity) context).startActivityForResult(enableBleIntent, REQUEST_ENABLE_BT);
-        } else {
-            if (Build.VERSION.SDK_INT >= 21) {
-                bleScanner = sensorBleAdapter.getBluetoothLeScanner();
-                bleSettings = new ScanSettings.Builder()
-                        .setScanMode(ScanSettings.SCAN_MODE_LOW_LATENCY)
-                        .build();
-                scanLeDevice(true);
-            }
+        if (Build.VERSION.SDK_INT >= 21) {
+            bleScanner = sensorBleAdapter.getBluetoothLeScanner();
+
+            bleSettings = new ScanSettings.Builder()
+                    .setScanMode(ScanSettings.SCAN_MODE_LOW_POWER)
+                    .build();
+
+            bleScanResults = new HashMap<>();
+            bleScanCallback = new BleScanCallback(bleScanResults);
+
+            startScan(true);
         }
     }
 
-    @RequiresApi(api = Build.VERSION_CODES.JELLY_BEAN_MR2)
-    private void scanLeDevice(final boolean enable) {
+
+    @RequiresApi(api = Build.VERSION_CODES.M)
+    private void startScan(final boolean enable) {
         if (enable) {
-//            bleHandler.postDelayed(new Runnable() {
-//                @Override
-//                public void run() {
-//                    if (Build.VERSION.SDK_INT < 21) {
-//                        sensorBleAdapter.stopLeScan(bleScanCallback);
-//                    } else {
-//                        bleScanner.stopScan(scanCallback);
-//                    }
-//                }
-//            }, SCAN_PERIOD);
-
-            if (Build.VERSION.SDK_INT < 21) {
-               sensorBleAdapter.startLeScan(bleScanCallback);
-            } else {
-                bleScanner.startScan(bleFilters, bleSettings, scanCallback);
+            if (!hasPermissions() || bleScanning) {
+                return;
             }
+            bleHandler = new Handler();
+            bleHandler.postDelayed(this::stopScan, SCAN_PERIOD);
+            bleScanner.startScan(bleFilters, bleSettings, bleScanCallback);
+            bleBluetoothLeScanner = sensorBleAdapter.getBluetoothLeScanner();
+            bleScanning = true;
         } else {
-            if (Build.VERSION.SDK_INT < 21) {
-                sensorBleAdapter.stopLeScan(bleScanCallback);
-            } else {
-                bleScanner.stopScan(scanCallback);
-            }
+            bleScanner.stopScan(bleScanCallback);
         }
     }
 
-    private BluetoothGattCallback bleCalback = new BluetoothGattCallback() {
-        @Override
-        public void onPhyUpdate(BluetoothGatt gatt, int txPhy, int rxPhy, int status) {
-            super.onPhyUpdate(gatt, txPhy, rxPhy, status);
+    @RequiresApi(api = Build.VERSION_CODES.M)
+    private boolean hasPermissions() {
+        if (sensorBleAdapter == null || !sensorBleAdapter.isEnabled()) {
+            requestBluetoothEnable();
+            return false;
+        } else if (!hasLocationPermissions()) {
+            requestLocationPermission();
+            return false;
         }
+        return true;
+    }
 
-        @Override
-        public void onPhyRead(BluetoothGatt gatt, int txPhy, int rxPhy, int status) {
-            super.onPhyRead(gatt, txPhy, rxPhy, status);
-        }
+    @RequiresApi(api = Build.VERSION_CODES.M)
+    private boolean hasLocationPermissions() {
+        return context.checkSelfPermission(Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED;
+    }
+    @RequiresApi(api = Build.VERSION_CODES.M)
+    private void requestLocationPermission() {
+        ((DashboardActivity) context).requestPermissions(new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, REQUEST_FINE_LOCATION);
+    }
 
-        @RequiresApi(api = Build.VERSION_CODES.JELLY_BEAN_MR2)
-        @Override
-        public void onConnectionStateChange(BluetoothGatt gatt, int status, int newState) {
-            super.onConnectionStateChange(gatt, status, newState);
-
-            if (status == BluetoothGatt.GATT_FAILURE) {
-                disconnectGattServer();
-                return;
-            } else if (status != BluetoothGatt.GATT_SUCCESS) {
-                disconnectGattServer();
-                return;
-            }
-            if (newState == BluetoothProfile.STATE_CONNECTED) {
-                // mConnected = true;
-            } else if (newState == BluetoothProfile.STATE_DISCONNECTED) {
-                disconnectGattServer();
-            }
-
-            if (newState == BluetoothProfile.STATE_CONNECTED) {
-                devicesList.add(gatt.getDevice());
-            }
-            if (newState == BluetoothProfile.STATE_DISCONNECTED) {
-                devicesList.remove(gatt.getDevice());
-            }
-        }
-
-        @RequiresApi(api = Build.VERSION_CODES.JELLY_BEAN_MR2)
-        @Override
-        public void onServicesDiscovered(BluetoothGatt gatt, int status) {
-            super.onServicesDiscovered(gatt, status);
-
-            BluetoothGattService bleService = gatt.getService(FARASENSE_CHARACTERISTIC_UUID_SENSOR_1);
-            bleCharacteristic = bleService.getCharacteristic(FARASENSE_CHARACTERISTIC_UUID_SENSOR_1);
-        }
-
-        @Override
-        public void onCharacteristicRead(BluetoothGatt gatt, BluetoothGattCharacteristic characteristic, int status) {
-            super.onCharacteristicRead(gatt, characteristic, status);
-        }
-
-        @Override
-        public void onCharacteristicWrite(BluetoothGatt gatt, BluetoothGattCharacteristic characteristic, int status) {
-            super.onCharacteristicWrite(gatt, characteristic, status);
-        }
-
-        @Override
-        public void onCharacteristicChanged(BluetoothGatt gatt, BluetoothGattCharacteristic characteristic) {
-            super.onCharacteristicChanged(gatt, characteristic);
-        }
-
-        @Override
-        public void onDescriptorRead(BluetoothGatt gatt, BluetoothGattDescriptor descriptor, int status) {
-            super.onDescriptorRead(gatt, descriptor, status);
-        }
-
-        @Override
-        public void onDescriptorWrite(BluetoothGatt gatt, BluetoothGattDescriptor descriptor, int status) {
-            super.onDescriptorWrite(gatt, descriptor, status);
-        }
-
-        @Override
-        public void onReliableWriteCompleted(BluetoothGatt gatt, int status) {
-            super.onReliableWriteCompleted(gatt, status);
-        }
-
-        @Override
-        public void onReadRemoteRssi(BluetoothGatt gatt, int rssi, int status) {
-            super.onReadRemoteRssi(gatt, rssi, status);
-        }
-
-        @Override
-        public void onMtuChanged(BluetoothGatt gatt, int mtu, int status) {
-            super.onMtuChanged(gatt, mtu, status);
-        }
-    };
-
-    public void disconnectGattServer() {
-//        mConnected = false;
-//        if (mGatt != null) {
-//            mGatt.disconnect();
-//            mGatt.close();
-//        }
+    private void requestBluetoothEnable() {
+        Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+        ((DashboardActivity) context).startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);
+        Log.d(TAG, "Requested user enables Bluetooth. Try starting the scan again.");
     }
 
     @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
-    private ScanCallback scanCallback = new ScanCallback() {
-        @Override
-        public void onScanResult(int callbackType, ScanResult result) {
-            super.onScanResult(callbackType, result);
-
-            BluetoothDevice btDevice = result.getDevice();
-
-            if (btDevice != null)
-                Log.i("BLE DEVICE: ", btDevice.toString());
-
-            if (btDevice.toString().equals(FARASENSE_SENSOR_1) && !bleDeviceConnected) {
-                btDevice.connectGatt(context, true, bleCalback);
-                bleScanner.stopScan(scanCallback);
-                bleDeviceConnected = true;
-            }
+    private void stopScan() {
+        if (bleScanning && sensorBleAdapter != null && sensorBleAdapter.isEnabled()) {
+            bleBluetoothLeScanner.stopScan(bleScanCallback);
+            scanComplete();
         }
 
-        @Override
-        public void onBatchScanResults(List<ScanResult> results) {
-            super.onBatchScanResults(results);
-            for (ScanResult result : results) {
-                Log.i("ScanCallback", "Results: "+ result.toString());
-            }
-        }
+        bleScanCallback = null;
+        bleScanning = false;
+        bleHandler = null;
+    }
 
-        @Override
-        public void onScanFailed(int errorCode) {
-            super.onScanFailed(errorCode);
-            Log.e("ScanCallback", "Failed, Error Code: " + errorCode);
+    private void scanComplete() {
+        if (bleScanResults.isEmpty()) {
+            return;
         }
-    };
-
-    @RequiresApi(api = Build.VERSION_CODES.JELLY_BEAN_MR2)
-    private BluetoothAdapter.LeScanCallback bleScanCallback = new BluetoothAdapter.LeScanCallback() {
-        @Override
-        public void onLeScan(BluetoothDevice device, int rssi, byte[] scanRecord) {
-            Log.i("BleScanCallback", "OnLeScan: " + device.toString());
+        for (Object deviceAddress : bleScanResults.keySet()) {
+            Log.d(TAG, "Found device: " + deviceAddress.toString());
         }
-    };
+    }
 }
